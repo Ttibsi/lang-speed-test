4d510e9ac8d20e9692a85bfa9fb0cb7232834b4a
ttibsi <ashisbitt@icloud.com>
2024-07-26 23:17:11 +0100

    finish chart generation first draft

--- charts.py
+++ charts.py
@@ -21,9 +21,13 @@ class Chart:
     y_label: str 
     file_name: str 
 
-    def to_img(self):
-
-        matplotlib.pyplot.savefig()
+    def to_img(self, chart: chart_t):
+        chart.title(self.title)
+        chart.xlabel(self.x_label)
+        chart.ylabel(self.y_label)
+        chart.savefig(self.file_name)
+        chart.close()
+        # matplotlib.pyplot.savefig()
 
 def main():
     if len(sys.argv) != 2:
@@ -35,11 +39,22 @@ def main():
     with open(f"example_csv.csv") as csv:
         frame: frame_t = pandas.read_csv(csv, index_col = "binary name")
 
-        # line graph of comparative sizes
-        line_plot: chart_t = frame["binary size (bytes)"].plot() 
-        bar_plot: chart_t  = frame.iloc[:,1:].plot().bar()
+        line_chart: Chart = Chart(
+            "Size Comparison",
+            "Language",
+            "Binary size",
+            "results/graphs/size_comparison.png"
+        )
 
+        bar_chart: Chart = Chart(
+            "Perf Comparison",
+            "Language",
+            "Time (seconds)",
+            "results/graphs/speed_comparison.png"
+        )
 
+        line_chart.to_img(frame["binary size (bytes)"].plot())
+        bar_chart.to_img(frame.iloc[:,1:].plot().bar())
 
 
 if __name__ == "__main__":

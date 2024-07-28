f230f93769ece09c4c8809db49fa8cafa0ea95db
ttibsi <ashisbitt@icloud.com>
2024-07-28 11:50:17 +0100

    Reformat to fix file generation

--- charts.py
+++ charts.py
@@ -1,18 +1,18 @@
 #!/bin/env python3
-
-from dataclasses import dataclass
+import os
 import sys
+from dataclasses import dataclass
 from typing import TypeAlias
 
 try:
-    import matplotlib
+    import matplotlib.pyplot
     import pandas
 except ModuleNotFoundError:
     print("Please ensure a virtualenv with pandas and matplotlib installed is present")
     sys.exit()
 
 frame_t: TypeAlias = pandas.core.frame.DataFrame
-chart_t: TypeAlias = matplotlib.axes..Axes
+chart_t: TypeAlias = matplotlib.pyplot.Axes
 
 @dataclass 
 class Chart:
@@ -20,41 +20,52 @@ class Chart:
     x_label: str 
     y_label: str 
     file_name: str 
+    bar_chart:bool
 
     def to_img(self, chart: chart_t):
-        chart.title(self.title)
-        chart.xlabel(self.x_label)
-        chart.ylabel(self.y_label)
-        chart.savefig(self.file_name)
-        chart.close()
-        # matplotlib.pyplot.savefig()
+        chart.set_title(self.title)
+        chart.set_xlabel(self.x_label)
+        chart.set_ylabel(self.y_label)
 
-def main():
+        if self.bar_chart:
+            chart.bar(0.3, 1)
+
+        chart.figure.savefig(self.file_name)
+        matplotlib.pyplot.close()
+
+def main() -> None:
     if len(sys.argv) != 2:
         print("Please select an architecture to read results from")
         return -1
 
+    if not os.path.exists("results/graphs"):
+        os.makedirs("results/graphs")
+
     arch: str = sys.argv[1]
-    # with open(f"results/{arch}/synthetic_results.csv") as csv:
-    with open(f"example_csv.csv") as csv:
+
+    with open(f"results/{arch}/synthetic_results.csv") as csv:
         frame: frame_t = pandas.read_csv(csv, index_col = "binary name")
 
         line_chart: Chart = Chart(
             "Size Comparison",
             "Language",
             "Binary size",
-            "results/graphs/size_comparison.png"
+            f"results/graphs/size_comparison_{arch}.png",
+            False
         )
 
         bar_chart: Chart = Chart(
             "Perf Comparison",
             "Language",
             "Time (seconds)",
-            "results/graphs/speed_comparison.png"
+            f"results/graphs/speed_comparison_{arch}.png",
+            True
         )
 
         line_chart.to_img(frame["binary size (bytes)"].plot())
-        bar_chart.to_img(frame.iloc[:,1:].plot().bar())
+        bar_chart.to_img(frame.iloc[:,1:].plot())
+
+    return
 
 
 if __name__ == "__main__":
